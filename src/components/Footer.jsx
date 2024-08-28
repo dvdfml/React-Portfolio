2496aaa13b80bc0e869aaa527285f60a50eedb30
David Miranda <dvdfermiranda@yahoo.com.mx>
2024-08-27 18:19:59 -0600

    Added header and footer components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+    return (
+        <div class="container">
+            <footer class="py-3 my-4">
+                <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+                    <li class="nav-item"><a href="https://github.com/dvdfml" class="nav-link px-2 text-muted">GitHub</a></li>
+                    <li class="nav-item"><a href="https://www.linkedin.com/" class="nav-link px-2 text-muted">LinkedIn</a></li>
+                </ul>
+            </footer>
+        </div>
+    );
+}
